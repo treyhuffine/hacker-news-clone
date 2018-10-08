0aa71307528760ba15d25496818a46366e829c8d
Trey Huffine <treyhuffine@gmail.com>
2018-10-08 12:08:42 -0400

    Add story to Redux

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { colorsDark } from 'styles/palette';
+import List from 'components/List';
+
+import { Wrapper, Title } from './styles';
+
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={colorsDark}>
+        <div>
+          <Wrapper>
+            <Title>Hacker News Reader</Title>
+            <List />
+          </Wrapper>
+        </div>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default App;
