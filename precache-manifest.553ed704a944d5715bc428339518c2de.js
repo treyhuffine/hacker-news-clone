1bb007e88914b8a7bc260a27d8e7faadf1305eea
Trey Huffine <treyhuffine@gmail.com>
2018-10-08 19:36:34 -0400

    Updates

---
+++ precache-manifest.553ed704a944d5715bc428339518c2de.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a98814c031d2c8aeed84",
+    "url": "/hacker-news-clone/static/js/runtime~main.a98814c0.js"
+  },
+  {
+    "revision": "0279bf9520ed62372637",
+    "url": "/hacker-news-clone/static/js/main.0279bf95.chunk.js"
+  },
+  {
+    "revision": "1bb912458768ed633bd4",
+    "url": "/hacker-news-clone/static/js/1.1bb91245.chunk.js"
+  },
+  {
+    "revision": "b0e7563f1064df02eed88324eddf7656",
+    "url": "/hacker-news-clone/index.html"
+  }
+];
